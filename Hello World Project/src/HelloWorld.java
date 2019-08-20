d8db23f646a632a6fdea0f466fe2aff7a7f5d114
APCS3rd <APCS3rd@MHS-LT9-18>
2019-08-20 10:12:31 -0700

    Complete Hello World project. We good.

---
+++ Hello World Project/src/HelloWorld.java
@@ -0,0 +1,12 @@
+//Gaby Offril
+//August 19, 2019
+//First class in JAVA for APCS A prints message to the screen
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+
+	}
+
+}
