f671f62eeed85f3b24b50b6a5c14ab2efa06b412
APCS3rd <APCS3rd@MHS-LT9-18>
2019-09-03 09:55:18 -0700

    Yeah so this is everything

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,12 @@
+//Gaby Offril
+//APCS Perid 3
+//August 29, 2019
+//Call the code from the Calculate class (aka client or runner code)
+public class DoMath {
+
+	public static void main(String[] args)
+	{
+		Calculate.square(5);
+	}
+	
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,15 @@
+//Gaby Offril
+//August 29, 2019
+//Library of math functions
+public class Calculate {
+	public static int square(int number) {
+		int answer-0; 
+		answer = number * number;
+		return answer;
+	}
+	
+	public static int cube(int number) {
+		int answer-0;
+		answer = 
+	}
+}
